88a9ccb74c0f79ce6f02451bb4f124e07434facc
NotPrashanthemm <notprashanthemm@gmail.com>
2025-05-14 19:15:21 +0530

    Basic application

---
+++ proj.py
@@ -0,0 +1,29 @@
+class chatbook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+
+
+    def menu(self):
+        user_input = input("""Welcome to chat book?
+                           1 to signup
+                           2 to signup
+                           3 to write a post
+                           4 to messege
+                           5 to exit""")
+        
+        if user_input == "1":
+            pass
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            pass
+        elif user_input == "4":
+            pass
+        else:
+            exit()
+
+
+obj = chatbook()
